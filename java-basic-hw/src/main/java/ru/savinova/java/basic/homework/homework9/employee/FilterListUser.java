8196e484e8bfa7241b1e314e1f4a5aea27a47086
smi <smi@labash.ru>
2024-02-22 15:41:47 +0300

    ДЗ 9 продолжение

--- java-basic-hw/src/main/java/ru/savinova/java/basic/homework/homework9/employee/FilterListUser.java
+++ java-basic-hw/src/main/java/ru/savinova/java/basic/homework/homework9/employee/FilterListUser.java
@@ -6,17 +6,43 @@ import java.util.List;
 
 public class FilterListUser {
     public List<String> getArrayOfName(List<User> users) {
-        List<String> arrayOfName =  new ArrayList<String>();
+        List<String> arrayOfName = new ArrayList<>();
         for (int i = 0; i < users.size(); i++) {
             arrayOfName.add(users.get(i).getName());
         }
         return arrayOfName;
     }
 
-//    public List<User> getOlder(List<User> users, int minAge) {
-//        Iterator<User> iter = users.iterator();
-//        while (iter.hasNext()) {
-//            }
-//        }
-//    }
+    public List<User> getOlder(List<User> users, int minAge) {
+        Iterator<User> iter = users.listIterator();
+        while (iter.hasNext()) {
+            User user = iter.next();
+            if (user.getAge() < minAge) {
+                iter.remove();
+            }
+        }
+        return users;
+    }
+
+    public void printList(List<User> arrayList) {
+        for (User users : arrayList) {
+            System.out.println(users.getName() + " " + users.getAge() + " годиков");
+        }
+    }
+
+    public Boolean isAverageAge(List<User> users, int minAverage) {
+        int averageAgeUsers = 0;
+        for (int i = 0; i < users.size(); i++) {
+            averageAgeUsers += users.get(i).getAge();
+        }
+        return ((averageAgeUsers / users.size()) > minAverage);
+    }
+
+    public void youngest(List<User> users) {
+        int minAge = 0;
+        for (int i = 0; i < users.size(); i++) {
+            users.get(i).getAge();
+        }
+        return;
+    }
 }
--- java-basic-hw/src/main/java/ru/savinova/java/basic/homework/homework9/employee/MainApp.java
+++ java-basic-hw/src/main/java/ru/savinova/java/basic/homework/homework9/employee/MainApp.java
@@ -6,11 +6,12 @@ import java.util.List;
 public class MainApp {
     public static void main(String[] args) {
         FilterListUser filterListUser = new FilterListUser();
-        List<User> users = new ArrayList<User>();
+        List<User> users = new ArrayList<>();
         users.add(new User("Dasha", 20));
         users.add(new User("Glasha", 30));
         users.add(new User("Bob", 40));
         System.out.println(filterListUser.getArrayOfName(users));
-
+        filterListUser.printList(filterListUser.getOlder(users, 30));
+        System.out.println(filterListUser.isAverageAge(users, 29));
     }
 }
