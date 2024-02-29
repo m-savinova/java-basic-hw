b295a1610ac2c21ba3bb334795fc8b7dd92ce769
smi <smi@labash.ru>
2024-02-29 17:27:34 +0300

    ДЗ 10 продолжение

--- java-basic-hw/src/main/java/ru/savinova/java/basic/homework/homework10/MainApplication.java
+++ java-basic-hw/src/main/java/ru/savinova/java/basic/homework/homework10/MainApplication.java
@@ -2,8 +2,10 @@ package ru.savinova.java.basic.homework.homework10;
 
 public class MainApplication {
     public static void main(String[] args) {
-        PhoneBook phoneBook = new PhoneBook();
-        phoneBook.add( "Иванов", "Иван", "Иванович", "81234567890");
-        phoneBook.getPhoneBook();
+        PhoneBook phoneBook = new PhoneBook("Петров Петр", "1234567890");
+        phoneBook.add("81234567890", "Иванов Иван");
+        phoneBook.add("81234567890", "Иванов Петр");
+        //phoneBook.getPhoneBook();
+        phoneBook.find("Иванов");
     }
 }
--- java-basic-hw/src/main/java/ru/savinova/java/basic/homework/homework10/PhoneBook.java
+++ java-basic-hw/src/main/java/ru/savinova/java/basic/homework/homework10/PhoneBook.java
@@ -2,17 +2,46 @@ package ru.savinova.java.basic.homework.homework10;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class PhoneBook {
-    Map<Name, String> mapPhoneBook = new HashMap<>();
+    private String name;
+    private String phone;
+    Map<String, String> mapPhoneBook = new HashMap<>();
 
-    public void add(String secondName, String name, String middleName, String phone) {
-        mapPhoneBook.put(new Name(secondName, name, middleName), phone);
+    public PhoneBook(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PhoneBook phoneBook = (PhoneBook) o;
+        return Objects.equals(name, phoneBook.name) && Objects.equals(phone, phoneBook.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, phone);
+    }
+
+    public void add(String phone, String name) {
+        mapPhoneBook.put(phone, name);
     }
 
     public void getPhoneBook() {
-        for (Map.Entry<Name, String> entry : mapPhoneBook.entrySet()) {
+        for (Map.Entry<String, String> entry : mapPhoneBook.entrySet()) {
             System.out.println(entry.getValue() + " " + entry.getKey());
+        }
+    }
+
+    public void find(String searchName) {
+        for (Map.Entry<String, String> entry : mapPhoneBook.entrySet()) {
+            if (entry.getValue().equals(searchName)) {
+                System.out.println(entry.getKey());
+            }
         }
     }
 }
