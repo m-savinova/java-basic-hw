a5d650066825f2b00361900526e80a7338729ac5
smi <beretta07@bk.ru>
2024-02-29 20:53:57 +0300

    ДЗ 10 выполнено

--- java-basic-hw/src/main/java/ru/savinova/java/basic/homework/homework10/MainApplication.java
+++ java-basic-hw/src/main/java/ru/savinova/java/basic/homework/homework10/MainApplication.java
@@ -2,10 +2,12 @@ package ru.savinova.java.basic.homework.homework10;
 
 public class MainApplication {
     public static void main(String[] args) {
-        PhoneBook phoneBook = new PhoneBook("Петров Петр", "1234567890");
+        PhoneBook phoneBook = new PhoneBook();
         phoneBook.add("81234567890", "Иванов Иван");
-        phoneBook.add("81234567890", "Иванов Петр");
-        //phoneBook.getPhoneBook();
+        phoneBook.add("78456845232", "Иванов Петр");
+        phoneBook.add("00000000123", "Стряпшин Bob");
+        phoneBook.getPhoneBook();
         phoneBook.find("Иванов");
+        phoneBook.containsPhoneNumber("00000000123");
     }
 }
--- java-basic-hw/src/main/java/ru/savinova/java/basic/homework/homework10/PhoneBook.java
+++ java-basic-hw/src/main/java/ru/savinova/java/basic/homework/homework10/PhoneBook.java
@@ -5,27 +5,7 @@ import java.util.Map;
 import java.util.Objects;
 
 public class PhoneBook {
-    private String name;
-    private String phone;
     Map<String, String> mapPhoneBook = new HashMap<>();
-
-    public PhoneBook(String name, String phone) {
-        this.name = name;
-        this.phone = phone;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PhoneBook phoneBook = (PhoneBook) o;
-        return Objects.equals(name, phoneBook.name) && Objects.equals(phone, phoneBook.phone);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, phone);
-    }
 
     public void add(String phone, String name) {
         mapPhoneBook.put(phone, name);
@@ -39,9 +19,20 @@ public class PhoneBook {
 
     public void find(String searchName) {
         for (Map.Entry<String, String> entry : mapPhoneBook.entrySet()) {
-            if (entry.getValue().equals(searchName)) {
-                System.out.println(entry.getKey());
+            String key = entry.getKey();
+            String value = entry.getValue();
+            if (value.contains(searchName)) {
+                System.out.println(key);
             }
         }
+    }
+
+    public boolean containsPhoneNumber(String num) {
+        if (mapPhoneBook.containsKey(num)) {
+            System.out.println("Телефон " + num + " есть в справочнике");
+            return true;
+        }
+        System.out.println("Телефон " + num + " отсутствует в справочнике");
+        return false;
     }
 }
