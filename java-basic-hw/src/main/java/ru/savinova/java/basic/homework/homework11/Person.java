6c8445902c707ebb3f83058eb3f1b7c13c826c8e
smi <smi@labash.ru>
2024-03-04 15:15:34 +0300

    ДЗ 11 начало

---
+++ java-basic-hw/src/main/java/ru/savinova/java/basic/homework/homework11/Person.java
@@ -0,0 +1,19 @@
+package ru.savinova.java.basic.homework.homework11;
+
+import javax.swing.text.Position;
+
+public class Person {
+    String name;
+    Position position;
+    Long id;
+
+    public Person(String name, Position position, Long id) {
+        this.name = name;
+        this.position = position;
+        this.id = id;
+    }
+
+
+
+
+}
---
+++ java-basic-hw/src/main/java/ru/savinova/java/basic/homework/homework11/PersonDataBase.java
@@ -0,0 +1,28 @@
+package ru.savinova.java.basic.homework.homework11;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PersonDataBase {
+
+    Map<Integer, String> base = new HashMap<>();
+
+    public Person findById(Long id) {
+
+        return null;
+    }
+
+    public void add(Person person) {
+
+    }
+
+    public boolean isManager(Person person) {
+
+        return false;
+    }
+
+    public boolean isEmployee(Long id) {
+
+        return false;
+    }
+}
