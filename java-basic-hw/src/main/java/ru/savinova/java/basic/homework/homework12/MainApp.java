719afca4034cb3899ab58378f72bdd4f4a89d683
smi <beretta07@bk.ru>
2024-03-13 23:31:53 +0300

    ДЗ 12 продолжение

--- java-basic-hw/src/main/java/ru/savinova/java/basic/homework/homework12/MainApp.java
+++ java-basic-hw/src/main/java/ru/savinova/java/basic/homework/homework12/MainApp.java
@@ -10,6 +10,7 @@ public class MainApp {
         File file = new File("java-basic-hw");
         System.out.println(Arrays.toString(file.listFiles()));
 
+
         Scanner input = new Scanner(System.in);
         System.out.println("Введите название файла с которым вы хотите работать: ");
         String name = input.nextLine();
@@ -18,7 +19,7 @@ public class MainApp {
         try (InputStreamReader in = new InputStreamReader(new FileInputStream("java-basic-hw/" + name))) {
             int n = in.read();
             while (n != -1) {
-                System.out.print((char)n);
+                System.out.print((char) n);
                 n = in.read();
             }
         } catch (IOException e) {
@@ -26,13 +27,18 @@ public class MainApp {
         }
         System.out.println("\n");
         System.out.println("Введите что вы там хотите записать в свой файл:");
-        String data = input.nextLine();
 
-        try (FileOutputStream out = new FileOutputStream("java-basic-hw/" + name, true)) {
-            byte[] buffer = data.getBytes(StandardCharsets.UTF_8);
-            out.write(buffer);
-        } catch (IOException e) {
-            e.printStackTrace();
+        String data = "";
+        while (!data.equals("exit")) {
+            data = input.nextLine();
+            try (FileOutputStream out = new FileOutputStream("java-basic-hw/" + name, true)) {
+                byte[] buffer = data.getBytes(StandardCharsets.UTF_8);
+                out.write(buffer);
+                System.out.println("Всё записано, если хотите закончить работу с файлом введите exit, если нет, " +
+                        "то вводите что вы там ещё хотите записать");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
